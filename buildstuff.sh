ec9b8bf1af60a5d2901880e9be06f9365df83f07
Julian Harty <julianharty@gmail.com>
2013-08-16 09:25:50 +0100

    Changed the build target to run the tests. I expect the tests to fail on travis-ci, if so I can start investigating why they fail. Also added an ignore for .lib so we don't include the generated libraries in the repo.

--- buildstuff.sh
+++ buildstuff.sh
@@ -2,7 +2,7 @@
 FAILED=0
 sudo mkdir /sdcard
 sudo chmod 777 /sdcard
-ant clean jar sources-jar
+ant clean jar build-and-test
 # simpler version of the build script used for the full DaisyReader project. Kept this structure for ease of integration.
 if [ "$?" = 1 ]; then
     echo "daisy-engine build failed!"
