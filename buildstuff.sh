7607787612d64dac7f31b43991a9f8ad7f0a527a
Julian Harty <julianharty@gmail.com>
2013-08-16 09:08:34 +0100

    First commit of the main files so I can initiate builds on travis-ci.

---
+++ buildstuff.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+FAILED=0
+sudo mkdir /sdcard
+sudo chmod 777 /sdcard
+ant clean jar sources-jar
+# simpler version of the build script used for the full DaisyReader project. Kept this structure for ease of integration.
+if [ "$?" = 1 ]; then
+    echo "daisy-engine build failed!"
+    FAILED=1
+fi
+exit $FAILED
+
